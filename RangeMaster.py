d5d0a4234ce6f9b4bd1efe23b06fccfcac9e4229
james <jameschapman17@gmail.com>
2023-05-11 21:53:16 -0700

    Working on manipulating fft data, finding a good range of frequencies on which to restrict ourselves.

--- RangeMaster.py
+++ RangeMaster.py
@@ -5,6 +5,87 @@ import matplotlib.pyplot as plt
 from pytube import YouTube
 from spleeter.separator import Separator
 import subprocess
+import wave
+
+import numpy as np
+import scipy.signal as signal
+def signal_frequency_range(audio_data,sample_rate):
+    # Assume audio_data is a 1D numpy array containing the audio samples
+    # Assume sample_rate is the sampling rate of the audio data
+
+    # Compute the power spectrum of the audio signal using the FFT
+    frequencies, power_spectrum = signal.welch(audio_data, sample_rate, nperseg=1024)
+    power_spectrum=np.sqrt(np.multiply(power_spectrum[:,0],power_spectrum[:,0])+np.multiply(power_spectrum[:,1],power_spectrum[:,1]))
+    print("power spectrum dimensions")
+    print(power_spectrum.size)
+    print(power_spectrum)
+    # Find the mean and standard deviation of the power spectrum
+    mean_spectrum = np.mean(power_spectrum)
+    print("mean_spectrum is "+str(mean_spectrum))
+    std_spectrum = np.std(power_spectrum)
+
+    # Find the upper and lower bounds for the power spectrum within two standard deviations
+    upper_bound = mean_spectrum + 2 * std_spectrum
+    print('upper_bound = ' + str(upper_bound))
+    lower_bound = mean_spectrum - 2 * std_spectrum
+    print('lower_bound = ' + str(lower_bound))
+
+    # Find the frequency range within the bounds
+    indices = np.where((power_spectrum <= upper_bound) & (power_spectrum >= lower_bound))[0]
+    print("indices")
+    print(indices.size)
+    print(indices)
+    actual_frequencies = np.fft.fftfreq(len(power_spectrum), 1/sample_rate)
+    frequencies=actual_frequencies
+    print("frequency")
+    print(frequencies)
+
+    min_frequency = frequencies[indices[0]]
+    max_frequency = frequencies[indices[-1]]
+
+    # Print the frequency range
+    print("Frequency range within the first two standard deviations: [{:.2f} Hz, {:.2f} Hz]".format(min_frequency, max_frequency))
+    return (min_frequency,max_frequency)
+
+
+def wav_data_extraction(wav_path):
+    # Open the WAV file
+    with wave.open(wav_path, "rb") as wav_file:
+        # Get the number of audio channels and the sample rate
+        num_channels = wav_file.getnchannels()
+        sample_rate = wav_file.getframerate()
+
+        # Read all the audio samples from the WAV file
+        audio_data = wav_file.readframes(wav_file.getnframes())
+
+        # Convert the audio data to a numpy array
+        audio_samples = np.frombuffer(audio_data, dtype=np.int16)
+
+        # Reshape the audio samples to a 2D array, where each row represents a channel
+        audio_samples = audio_samples.reshape((-1, num_channels))
+
+    # Get the duration of the WAV file in seconds
+    duration = len(audio_samples) / sample_rate
+
+    # Create a time axis in seconds
+    time_axis = np.linspace(0, duration, len(audio_samples))
+
+    # # Plot the waveform for each channel
+    # for i in range(num_channels):
+    #     plt.plot(time_axis, audio_samples[:, i])
+
+    # # Add labels and title
+    # plt.xlabel("Time (s)")
+    # plt.ylabel("Amplitude")
+    # plt.title("Waveform")
+
+    # # Show the plot
+    # plt.show()
+    return (audio_samples, sample_rate)
+
+
+
+
 
 
 def mp4_to_wav(filename_mp4,filename_wav):
@@ -31,15 +112,15 @@ def download_YT_mp3_by_url(url):
     # download the audio
     stream.download()
 
-def vocal_splitter(wav_path):
+def vocal_splitter(audio_data):
     # Load the pre-trained vocal separation model
     separator = Separator('spleeter:2stems')
 
     # # Load the audio file
-    # waveform, sample_rate = separator.load_audio(wav_path)
+    # waveform, sample_rate = sepagtrator.load_audio(wav_path)
 
     # Separate the vocals from the audio
-    prediction = separator.separate(wav_path)
+    prediction = separator.separate(audio_data)
 
     # Extract the vocals from the prediction dictionary
     vocals = prediction['vocals']
@@ -59,27 +140,25 @@ def histogram(vocals):
 
 
 
+def power_spectral_density(audio_data, sample_rate):
 
 
-# # Load the audio file
-# sample_rate, audio_data = wavfile.read('path/to/audio/file')
+    # Compute the FFT of the audio signal
+    fft_data = np.fft.fft(audio_data)
 
-# # Compute the FFT of the audio signal
-# fft_data = np.fft.fft(audio_data)
+    # Compute the power spectral density (PSD) of the FFT
+    psd_data = np.abs(fft_data) ** 2
 
-# # Compute the power spectral density (PSD) of the FFT
-# psd_data = np.abs(fft_data) ** 2
+    # Compute the frequency range corresponding to each FFT bin
+    freqs = np.fft.fftfreq(len(psd_data), 1/sample_rate)
 
-# # Compute the frequency range corresponding to each FFT bin
-# freqs = np.fft.fftfreq(len(psd_data), 1/sample_rate)
+    # Find the range of frequencies that contain the highest signal power
+    max_freq_range = freqs[np.argmax(psd_data)]  # Peak frequency
+    low_freq_range = freqs[np.argmin(np.abs(psd_data[:len(psd_data)//2] - max(psd_data)/2))]  # Lower frequency range
+    high_freq_range = freqs[np.argmin(np.abs(psd_data[len(psd_data)//2:] - max(psd_data)/2)) + len(psd_data)//2]  # Higher frequency range
 
-# # Find the range of frequencies that contain the highest signal power
-# max_freq_range = freqs[np.argmax(psd_data)]  # Peak frequency
-# low_freq_range = freqs[np.argmin(np.abs(psd_data[:len(psd_data)//2] - max(psd_data)/2))]  # Lower frequency range
-# high_freq_range = freqs[np.argmin(np.abs(psd_data[len(psd_data)//2:] - max(psd_data)/2)) + len(psd_data)//2]  # Higher frequency range
-
-# # Print the results
-# print(f"Frequency range: {low_freq_range} Hz - {high_freq_range} Hz")
+    # Print the results
+    print(f"Frequency range: {low_freq_range} Hz - {high_freq_range} Hz")
 
 
 
@@ -107,8 +186,10 @@ def main():
     # download_YT_mp3_by_url_ui()
     # audio_file=r"C:\Projects\RangeMaster\test.mp4"
     # mp4_to_wav(audio_file,r"C:\Projects\RangeMaster\test.wav")
-    vocals=vocal_splitter(r"C:\Projects\RangeMaster\test.wav")
-    histogram(vocals)
+    audio_data, sample_rate=wav_data_extraction(r"C:\Projects\RangeMaster\test.wav")
+    vocals=vocal_splitter(audio_data)
+    print(sample_rate)
+    signal_frequency_range(vocals,sample_rate)
 
 if __name__ == '__main__':
     main()
